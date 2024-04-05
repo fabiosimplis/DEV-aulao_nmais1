2464448aa04198e6eeab5b4bb2fa4ad31231598c
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-05 12:48:10 -0300

    Melhorando consulta de muitos para muitos

--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT p FROM Product p JOIN FETCH p.categories WHERE p IN :products " )
+    List<Product> searchProductsCategories(List<Product> products);
 }
